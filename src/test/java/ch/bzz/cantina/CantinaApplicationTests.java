c5a3a11620f03364ba7a6b135a7e48537a97800c
Dennis Künzle <denall@gmx.ch>
2022-06-02 11:35:22 +0200

    initial commit

---
+++ src/test/java/ch/bzz/cantina/CantinaApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bzz.cantina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
